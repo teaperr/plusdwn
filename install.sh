415062eef0918a886de82e518aa345218431f0fa
teaperr <143550106+teaperr@users.noreply.github.com>
2024-02-13 04:11:13 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+install_dir="$HOME/bin"
+
+mkdir -p "$install_dir"
+curl -sSL https://raw.githubusercontent.com/teaperr/plusdwn/main/plusdwn -o "$install_dir/plusdwn"
+chmod +x "$install_dir/yourscript.sh"
+
+echo "" >> ~/.bashrc
+echo "export PATH="$HOME/bin:$PATH"" >> ~/.bashrc
+source ~/.bashrc
+
+echo "Installation complete. You can now use plusdwn!"
