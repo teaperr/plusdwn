bbe859148dd0b0c48324b08dc760ca8d6e3f5c0b
teaperr <143550106+teaperr@users.noreply.github.com>
2024-02-13 04:36:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+line='export PATH="$HOME/bin:$PATH"'
 install_dir="$HOME/bin"
 
 mkdir -p "$install_dir"
 curl -sSL https://raw.githubusercontent.com/teaperr/plusdwn/main/plusdwn -o "$install_dir/plusdwn"
 chmod +x "$install_dir/plusdwn"
 
-echo "" >> ~/.bashrc
-echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
+if ! grep -qF "$line" ~/.bashrc; then
+    echo "$line" >> ~/.bashrc
+fi
+
 source ~/.bashrc
 
 echo "Installation complete. You can now use plusdwn!"
