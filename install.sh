549c8ebc345b19afcdd36c9a509fb0e996396bf3
teaperr <143550106+teaperr@users.noreply.github.com>
2024-02-13 04:29:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ install_dir="$HOME/bin"
 
 mkdir -p "$install_dir"
 curl -sSL https://raw.githubusercontent.com/teaperr/plusdwn/main/plusdwn -o "$install_dir/plusdwn"
-chmod +x "$install_dir/yourscript.sh"
+chmod +x "$install_dir/plusdwn"
 
 echo "" >> ~/.bashrc
 echo "export PATH="$HOME/bin:$PATH"" >> ~/.bashrc
